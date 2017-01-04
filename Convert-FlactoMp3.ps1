2843056d4c127afa0a5c5886661ba4ffb2b64636
Nick G-W <nickgw@users.noreply.github.com>
2017-01-04 10:50:58 -0500

    initial commit of Convert-FlactoMp3
    Script to convert flacs to mp3

---
+++ Convert-FlactoMp3.ps1
@@ -0,0 +1,11 @@
+﻿$flacloc = "d:\users\nick.admin\Downloads\ffmpeg-20161230-6993bb4-win64-static\bin"
+#$flacloc = "$env:USERPROFILE\Downloads\ffmpeg-20161230-6993bb4-win64-static\bin"
+
+$flacs = Get-ChildItem | Where-Object {$_.name.toupper().EndsWith(".FLAC")}
+
+foreach ($flac in $flacs)
+{
+
+    & "$flacloc\ffmpeg.exe" -i $flac -ab 320k -map_metadata 0 -id3v2_version 3 $flac.name.Replace(".flac",".mp3")
+
+}
